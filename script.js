7d2563ec1dec20ed919c76b4643ffb04944e9e3d
Krejcza <155578815+Krejcza@users.noreply.github.com>
2024-06-04 19:39:14 +0200

    page transition

---
+++ script.js
@@ -0,0 +1,59 @@
+function pageTrans (){
+   let tl = gsap.timeline();
+
+   tl.to('.transition', {
+      duration: 1,
+      scaleY: 1,
+      transformOrigin: 'bottom',
+      ease: 'power4.inOut',
+   });
+
+   tl.to('.transition', {
+      duration: 1,
+      scaleY: 0,
+      transformOrigin: 'top',
+      ease: 'power4.inOut',
+      delay: 0.2,
+   });
+}
+
+function contentAnim(){
+   let lt = gsap.timeline();
+   lt.to('h1', {
+      top: 0,
+      duration: 1,
+      ease: 'power3.inOut',
+      delay: 0.75,
+   });
+}
+
+function delay(n){
+   n = n || 0;
+   return new Promise((done)=>{
+      setTimeout(()=>{
+         done();
+      }, n);
+   });
+}
+
+barba.init({
+   sync: true,
+   transitions: [
+      {
+         async leave(data){
+            const done = this.async();
+
+            pageTrans();
+            await delay(1000);
+            done();
+         },
+
+         async enter(data){
+            contentAnim();
+         },
+         async once(data){
+            contentAnim();
+         }
+      }
+   ]
+});
